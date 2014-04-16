e39f1a288219bfa27fee95567933c4ab60be750c
twneale <twneale@gmail.com>
2014-04-16 15:29:02 -0400

    So much views wow

--- app/views.py
+++ app/views.py
@@ -9,6 +9,7 @@ from django.core import urlresolvers, mail
 from django.views.generic import edit
 from django.views.decorators.debug import sensitive_post_parameters
 from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
 
 from django.template import loader
 from django.template.response import TemplateResponse
@@ -18,6 +19,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 
 from django.contrib.auth import get_user_model, authenticate, login, logout
+from django.contrib.auth.views import password_change
 from django.contrib.auth.forms import SetPasswordForm
 from django.contrib.auth.models import Group, Permission
 from django.contrib.auth.tokens import default_token_generator
@@ -25,11 +27,16 @@ from django.contrib.auth.decorators import (
     user_passes_test, login_required, permission_required)
 from django.contrib.contenttypes.models import ContentType
 
+from django.contrib.sites.models import get_current_site
+
 from . import forms
-from .models import Message, LfrUser
+from .models import Message
 
 
 class GeoLookup(edit.FormView):
+    '''Currently just looks up people by name in the OCD API, but
+    later will get people for lat/long.
+    '''
     template_name = 'app/geo_lookup.html'
     form_class = forms.GeoLookupForm
 
@@ -45,6 +52,8 @@ class GeoLookup(edit.FormView):
 
 
 class ComposeMessage(edit.CreateView):
+    '''Handles the display and processing of the message form.
+    '''
     model = Message
     form_class = forms.MessageForm
 
@@ -76,16 +85,19 @@ class ComposeMessage(edit.CreateView):
             User = get_user_model()
             user, created = User.objects.get_or_create(email=email)
             if created:
-                # Set a dummy password and log the new user in.
+                # Set a dummy password and authenticate the new user.
                 user.set_password('doggy')
                 user.save()
                 user = authenticate(email=user.email, password='doggy')
+
+                # Make the password unusable and log the new user in.
                 user.set_unusable_password()
+                user.save()
                 login(self.request, user)
             else:
                 # Here the user exists but isn't logged in.
                 # They'll get prompted for passwd at authorize_message.
-                # Make a email address available to the login view.
+                # Make email address available to the login view.
                 self.request.session['email'] = email
 
         # Trigger the form save.
@@ -100,9 +112,9 @@ class ComposeMessage(edit.CreateView):
 @login_required
 @permission_required('app.add_message', login_url=settings.VERIFY_EMAIL_URL)
 def confirm_message_sent(request, message_id):
-    '''This ensures the user is authenticated, has been prompted for a
-    password, and has a verified email address. After that, it sends
-    the message request off to earwig.
+    '''This ensures the user is authenticated and has a verified email
+    address. The message has already been sent to earwig, and now we're
+    just prompting the user to verify their email.
     '''
     message = Message.objects.get(id=message_id)
     ctx = dict(message=message)
@@ -110,15 +122,17 @@ def confirm_message_sent(request, message_id):
 
 
 def email_verification_prompt(request):
-    '''Send the email to start email verification.
-
+    '''Sends the email to start email verification.
     See django.contrib.auth.forms.PasswordResetForm
     '''
+    current_site = get_current_site(request)
+    site_name = current_site.name
+    domain = current_site.domain
     ctx = {
         'email': request.user.email,
         'uid': urlsafe_base64_encode(force_bytes(request.user.pk)),
         'user': request.user,
-        'domain': settings.LFR_DOMAIN,
+        'domain': domain,
         'token': default_token_generator.make_token(request.user),
         'protocol': 'https' if request.is_secure() else 'http',
     }
@@ -128,7 +142,7 @@ def email_verification_prompt(request):
     subject = ''.join(subject.splitlines())
     body_txt = loader.render_to_string(email_template + '.txt', ctx)
     body_html = loader.render_to_string(email_template + '.html', ctx)
-    from_email = settings.LFR_EMAIL_SENDER
+    from_email = None # settings.DEFAULT_FROM_EMAIL
     to = request.user.email
 
     msg = mail.EmailMultiAlternatives(subject, body_txt, from_email, [to])
@@ -143,8 +157,7 @@ def email_verification_confirmation(request, uidb64=None, token=None,
        token_generator=default_token_generator,
        post_reset_redirect=None,
        current_app=None, extra_context=None):
-    '''User visited the email verification link.
-
+    '''User clicked the email verification link.
     See django.contrib.auth.view.password_reset_confirm
     '''
     UserModel = get_user_model()
@@ -180,6 +193,11 @@ def email_verification_confirmation(request, uidb64=None, token=None,
 
 
 def email_verification_complete(request):
+    '''This is the redirect after the user clicks the confirmation link
+    in the verification email. If the user was diverted to email verification
+    while trying to send a message, they get redirected to the message
+    confirmation.
+    '''
     if not request.user.is_authenticated:
         return render(request, 'app/verify_email_complete.html', {})
 
@@ -187,15 +205,22 @@ def email_verification_complete(request):
     messages.success(request, 'Your account was successfully activated.')
 
     # If no password set yet, suggest setting password.
-    if request.user.has_unusable_password():
-        tmpl = '''
-            To make logging in easier next time, please
-            <a href="{% url 'django.contrib.auth.views.password_reset' %}">
-            set your password.</a>
-            '''
-        messages.warning(request, loader.render_to_string(tmpl))
+    if not request.user.has_usable_password():
+        messages.warning(
+            request, loader.render_to_string('app/_please_set_password.html'))
 
     # Forward to the pending message.
     message_id = request.session.pop('message_id')
     return redirect(
         urlresolvers.reverse('confirm_message_sent', args=(message_id,)))
+
+
+@sensitive_post_parameters()
+@csrf_protect
+@login_required
+def password_set(request):
+    '''Allow the user to set a usable password.
+    '''
+    return password_change(request,
+        template_name='registration/password_set_form.html',
+        password_change_form=SetPasswordForm)
