669fa9e091036f073ae56c3181f46b055a8ae38c
twneale <twneale@gmail.com>
2014-04-16 15:28:31 -0400

    Add some docs for custom auth models

--- app/models.py
+++ app/models.py
@@ -70,30 +70,25 @@ class Message(models.Model):
 post_save.connect(Message.send_to_earwig, sender=Message)
 
 
+# ----------------------------------------------------------------------------
+# Custom user stuff
+# ----------------------------------------------------------------------------
 class LfrUserManager(BaseUserManager):
     def create_user(self, email, password=None):
-        """
-        Creates and saves a User with the given email and password.
+        """Creates and saves a User with the given email and password.
         """
         if not email:
             raise ValueError('Users must have an email address')
-
-        user = self.model(
-            email=self.normalize_email(email),
-        )
-
+        user = self.model(email=self.normalize_email(email))
         user.set_password(password)
         user.save(using=self._db)
         return user
 
     def create_superuser(self, email, password):
-        """
-        Creates and saves a superuser with the given email, date of
+        """Creates and saves a superuser with the given email, date of
         birth and password.
         """
-        user = self.create_user(email,
-            password=password,
-        )
+        user = self.create_user(email, password=password)
         user.is_admin = True
         user.save(using=self._db)
         return user
