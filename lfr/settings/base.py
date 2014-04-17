22fe9be2d8435c1d9adafd7c082c6f8d04f92481
twneale <twneale@gmail.com>
2014-04-17 11:02:32 -0400

    Moved sqlite database setting in settings.dev

--- lfr/settings/base.py
+++ lfr/settings/base.py
@@ -58,16 +58,6 @@ ROOT_URLCONF = 'lfr.urls'
 
 WSGI_APPLICATION = 'lfr.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 LANGUAGE_CODE = 'en-us'
--- lfr/settings/dev.py
+++ lfr/settings/dev.py
@@ -9,6 +9,16 @@ TEMPLATE_DEBUG = DEBUG
 settings.INTERNAL_IPS += ('localhost', '127.0.0.1')
 settings.ALLOWED_HOSTS += ['localhost', '127.0.0.1']
 
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
 # Comment this out to enable postmark email sending.
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
